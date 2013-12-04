2b8a554a04c9ca34fb5f38c9c45afd549102b153
Irene Lobo Valbuena <airinilv@gmail.com>
2013-12-04 16:08:53 +0100

    Reading parameterised rows OK. Some parsers for comments + properties included, might not use them if we take the simpler file formats

--- ReadGOL.hs
+++ ReadGOL.hs
@@ -5,17 +5,6 @@ import Parsing
 import Data.Char
 import Data.Maybe
 
-readLife :: FilePath -> IO World
-readLife f = do
-               file <- readFile f
-               let txt = lines file
-                   (x,y) = case parse offset (head txt) of
-                                Just (p,"") -> p
-                                _           -> (0,0)
-               print (x,y)
-               return $ emptyWorld (10,10)
-
-
 --specString :: String -> Parser Char
 --specString s | length s == 1 = char (head s)
 --             | otherwise     = char (head s) >-> specString (tail s)
@@ -60,9 +49,64 @@ deadCell = char '.' >-> success False
 
 liveCell :: Parser Bool
 liveCell = char '*' >-> success True
+--liveCell = (char '*' +++ char '0') >-> success True
 
 row :: Parser [Bool]
 row = oneOrMore (deadCell +++ liveCell)
 
+ignore :: a -> Parser a
+ignore t = zeroOrMore (sat (\c -> True)) >-> return t
 
+dimensions = do char '('
+                c <- specString "cells "       >-> nat
+                l <- specString " length "     >-> nat
+                w <- specString " width "      >-> nat
+                g <- specString " generation " >-> nat
+                char ')'
+                return (c,l,w,g)
 
+patternName = do char '"'
+                 n <- oneOrMore $ sat (/= '"')
+                 char '"'
+                 return n
+
+comment = char '!' >-> do
+                         char ' '
+                         patternName
+                         char ' '
+                         t <- dimensions
+                         return t
+                       +++ ignore (0,0,0,0)
+
+numbered :: Parser [Bool]
+numbered = do
+             n <- nat
+             b <- (liveCell +++ deadCell)
+             return (replicate n b)
+
+paramRow :: Parser [Bool]
+paramRow = do
+             f <- row +++ numbered
+             g <- paramRow
+             return (f ++ g)
+           +++ return []
+
+readLife :: FilePath -> IO World
+readLife f = do
+               file <- readFile f
+               let txt = lines file
+                   (x,y) = case parse offset (head txt) of
+                                Just (p,"") -> p
+                                _           -> (0,0)
+               print (x,y)
+               return $ emptyWorld (10,10)
+
+{- File structure
+  * comments: start with "!"
+  * world general info: ! "some-string" (cells <nat> length <nat> width <nat> generation <nat>)
+  * offset: <nat>k<nat>h@!
+  * row: (<nat>)?("."+"0")*
+         trailing: dead
+  * empty row: "." == all dead
+
+-}
