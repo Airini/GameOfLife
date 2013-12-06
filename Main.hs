7b5838802b99a3106f3f2d707bccb37ae14f2ede
Attila <attilanagy85@gmail.com>
2013-12-06 12:03:40 +0100

    Main.hs was not added...

---
+++ Main.hs
@@ -0,0 +1,41 @@
+module Main where
+
+import GOL
+import World
+import Graphics.UI.GLUT
+import Data.IORef
+import Bindings
+import Display
+
+main :: IO ()
+main = do
+         let initW = blinker
+         runGame initW
+
+runGame :: (LiveCell a) => World a -> IO ()
+runGame w = do
+  (_progName, _args) <- getArgsAndInitialize
+  initialDisplayMode $= [WithDepthBuffer, DoubleBuffered]
+  _window <- createWindow "Hello World"
+  reshapeCallback $= Just reshape
+  world <- newIORef w
+  angle <- newIORef 0
+  zoom <- newIORef 0.5
+  delta <- newIORef 0.1
+  pos <- newIORef (0, 0)
+  addTimerCallback timerMs (redisplay)
+  keyboardMouseCallback $= Just (keyboardMouse zoom delta pos)
+  idleCallback $= Just (idle world angle delta)
+  displayCallback $= display world zoom angle pos
+  mainLoop
+{-runGame w | w == nextW  = printWorld w-}
+          {-| otherwise   = do-}
+                            {-printWorld w-}
+                            {-runGame nextW-}
+    where nextW = tick w
+
+redisplay :: TimerCallback
+redisplay = do
+    addTimerCallback timerMs (redisplay)
+
+timerMs = 500
