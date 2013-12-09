ed7a392f07d3c3280920c952fafbcded805b2a8c
Irene Lobo Valbuena <airinilv@gmail.com>
2013-12-09 10:27:14 +0100

    started test module: generators

---
+++ GOLTest.hs
@@ -0,0 +1,18 @@
+module GOLTest where
+
+import Test.QuickCheck
+import World
+import GOL
+import ReadGOL
+
+cell :: LiveCell a => Gen a
+cell = frequency [(9, return deadC), (2, return newlC)]
+
+instance LiveCell a => Arbitrary (World a) where
+  arbitrary =
+    do a <- arbitrary
+       d <- mapTuple ((+1) . (`mod` 200)) a
+       rows <- sequence [ sequence [ cell | i <- [1..fst d] ]
+                                   | j <- [1..snd d]]
+       return (World d rows)
+
